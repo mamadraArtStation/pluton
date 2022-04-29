5cc02a14ca890a36562eb5360445f0c8abc7dd06
Дмитрий Косарев <dmitryi@artstation.com>
2022-04-29 21:33:55 +0300

    added method for save username

--- pluton.py
+++ pluton.py
@@ -4,7 +4,6 @@ import discord
 import random
 import constant
 
-
 discordNew = discord
 
 intents = discord.Intents.default()
@@ -28,7 +27,7 @@ def checkAutorNotBot(message):
 def get_info(message):
     channel = client.get_channel(config['start_channel'])
     members = channel.members
-    users_array=[]
+    users_array = []
     for item in members:
         role_array = []
         for item_role in item.roles:
@@ -36,12 +35,10 @@ def get_info(message):
             role_array.append(role_id)
         user = {'user_id': item.id, 'user_roles': role_array}
         users_array.append(user)
-    users_json = {'users':users_array}
+    users_json = {'users': users_array}
     with open(file='./files/users.json', mode='w', encoding="utf-8") as file:
         json.dump(users_json, file, ensure_ascii=False)
     pass
-
-
 
 
 async def set_start_channel(message):
@@ -69,6 +66,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if checkAutorNotBot(message):
+        if message.content.startswith("-set_banner"):
+            print(message.guild.banner)
+            guild = message.guild
+            await guild.set_banner_url(message.attachment[0].proxy_url)
         if message.content.startswith("-get_info"):
             get_info(message)
         if message.content.startswith("-set_start_channel"):
@@ -84,35 +85,55 @@ async def on_member_join(member):
         for user in users_json['users']:
             if user['user_id'] == member.id:
                 roles_array = user['user_roles']
-    guild = member.guild
-    for role_id in roles_array:
-        role = guild.get_role(role_id)  # получаем объект роли*
-        if not role.name == '@everyone':
-            await member.add_roles(role)
+                display_name = user['display_name']
+                guild = member.guild
+                await member.edit(nick=display_name)
+                for role_id in roles_array:
+                    role = guild.get_role(role_id)  # получаем объект роли*
+                    if not role.name == '@everyone':
+                        await member.add_roles(role)
 
+
+def get_roles_array(member):
+    roles_array = []
+    for role_item in member.roles:
+        role = role_item.id
+        roles_array.append(role)
+    return roles_array
 
 
 @client.event
 async def on_member_remove(member):
     roles_array = []
+    new_user = True
+    display_name = member.display_name
     with open(file='./files/users.json', mode='r') as file:
         users_json = json.load(file)
         for user in users_json['users']:
             if user['user_id'] == member.id:
-                for role_item in member.roles:
-                    role = role_item.id
-                    roles_array.append(role)
-                user['user_roles'] = roles_array
+                new_users = False
+                roles_array = get_roles_array(member)
+                user['user_roles'] = get_roles_array(member)
+                try:
+                    user['display_name'] = display_name
+                except:
+                    users_json.remove(user)
+                    user_new = {'user_id': member.id,
+                                'user_roles': roles_array,
+                                'display_name': display_name}
+                    users_json.append(user_new)
+    if new_user:
+        user_new = {'user_id': member.id,
+                    'user_roles': get_roles_array(member),
+                    'display_name': display_name}
+        users_json['users'].append(user_new)
     with open(file='./files/users.json', mode='w', encoding="utf-8") as file:
         json.dump(users_json, file, ensure_ascii=False)
     print(member)
 
 
-
-
 workSpace = 'production'
 client.run(config['tokenDiscordProd'])
-
 
 # python3 -m pip install -U discord.py
 # pip install -U discord-py-slash-command
